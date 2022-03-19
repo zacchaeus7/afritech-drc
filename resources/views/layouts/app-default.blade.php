c704848e5b363c560fdf0d2bdb9dbe5eb273376f
= <=>
2022-03-19 23:37:06 +0200

    test

--- resources/views/layouts/app-default.blade.php
+++ resources/views/layouts/app-default.blade.php
@@ -29,7 +29,9 @@
     <link rel="stylesheet" type="text/css" href="{{url('assets/css/settings.css')}}" media="screen" />
 
     <!-- Vella CSS Styles  -->
+    @production
     <link rel="stylesheet" type="text/css" href="{{url('assets/css/style.css')}}" media="screen">
+    @endproduction
 
     <!-- Responsive CSS Styles  -->
     <link rel="stylesheet" type="text/css" href="{{url('assets/css/responsive.css')}}" media="screen">
